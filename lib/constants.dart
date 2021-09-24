870c3602c628bc4393b6050fc2f47525c07d0b05
Wesley Chiang <wesleychiang96@gmail.com>
2021-09-24 16:10:09 +0800

    update map and modification

---
+++ lib/constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class EmptyScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("This is a example of screen."),
+      ),
+    );
+  }
+}
