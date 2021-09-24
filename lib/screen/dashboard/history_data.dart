0cc214294bf909c32388a80f434d242e74b19bb7
Wesley Chiang <wesleychiang96@gmail.com>
2021-09-24 14:47:18 +0800

    test

--- lib/screen/dashboard/history_data.dart
+++ lib/screen/dashboard/history_data.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sunshine/screen/models/climatology.dart';
 import 'dart:async';
+import 'package:sunshine/screen/models/response.dart' as data;
 
 
 class SummaryScreen extends StatefulWidget {
@@ -11,10 +12,6 @@ class SummaryScreen extends StatefulWidget {
 class _HistoryDataState extends State<HistoryData> {
 
   Future<Map<String, dynamic>> _futureData;
-
-  // Now the following variables only serve as helpers or convinient variables.
-  // So that we don't need to change our previous code so much.
-  // They will be updated in the FutureBuilder (once the fetch process completes)
 
   late List<Climatology> climatology;
 
@@ -38,90 +35,3 @@ class _HistoryDataState extends State<HistoryData> {
         });
   }
 
-  Scaffold _buildMainScreen() {
-    return Scaffold(
-      appBar: AppBar(
-        title: Column(
-          children: <Widget>[
-            const Text(
-              'Peer and Self Assessement by',
-              style: TextStyle(fontSize: 15),
-            ),
-            Text(
-              evaluator.fullName,
-              style: const TextStyle(fontSize: 25),
-            ),
-          ],
-        ),
-      ),
-      body: ListView.separated(
-        itemCount: assessments.length,
-        itemBuilder: (context, index) => _ListTile(
-          index: index,
-          screen: this,
-        ),
-        separatorBuilder: (context, index) => Divider(
-          color: Colors.grey,
-        ),
-      ),
-    );
-  }
-
-  Scaffold _buildFetchingDataScreen() {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            CircularProgressIndicator(),
-            SizedBox(height: 50),
-            Text('Fetching data in progress'),
-          ],
-        ),
-      ),
-    );
-  }
-} // class _SummaryScreenState
-
-class _ListTile extends StatefulWidget {
-  final int index;
-  final _SummaryScreenState
-  screen; // A convinience variable to access data from the screen object, so that no need to pass multiple variables
-
-  _ListTile({this.index, this.screen});
-
-  @override
-  __ListTileState createState() => __ListTileState();
-}
-
-class __ListTileState extends State<_ListTile> {
-  void _navigate() async {
-    final result = await Navigator.pushNamed(context, detailsRoute, arguments: {
-      'assessment': Assessment.copy(widget.screen.assessments[widget.index]),
-      'criteria': widget.screen.criteria,
-      'scales': widget.screen.scales
-    });
-
-    if (result != null) {
-      setState(() => widget.screen.assessments[widget.index] = result);
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(widget.screen.assessments[widget.index].member.shortName),
-      subtitle: Text(widget.screen.assessments[widget.index].member.fullName),
-      trailing: CircleAvatar(
-        child: Text(
-          widget.screen.assessments[widget.index].percent.round().toString(),
-          style: TextStyle(color: Colors.white),
-        ),
-        backgroundColor: widget.screen.assessments[widget.index].percent < 50
-            ? Colors.pink
-            : Colors.purple,
-      ),
-      onTap: () => _navigate(),
-    );
-  }
-}
